f30f1ac80028cbe620da9d22e950384de3524e20
thaihust <thaiph.hust@gmail.com>
2017-06-08 14:57:23 +0700

    modify vpn diagram

--- VyOS-E2E-Websrv/scripts/vFW/iptables-sample-config/iptables_rules.sh
+++ VyOS-E2E-Websrv/scripts/vFW/iptables-sample-config/iptables_rules.sh
@@ -12,7 +12,9 @@ iptables -A FORWARD -i $FW_INT_PUB_NAME -o $FW_INT_DMZ_NAME -p tcp --dport 80 -d
 iptables -t nat -A POSTROUTING -s $DMZ_NET -o $FW_INT_PUB_NAME -j SNAT --to-source $FW_INT_PUB_ADDR
 
 # for wan optimizer behind firewall
-iptables -t nat -I PREROUTING -i $FW_INT_PUB_NAME -p tcp --dport 443 -j DNAT --to-destination $WEBSRV_INT_DMZ_ADDR
-iptables -t nat -I POSTROUTING -o $FW_INT_DMZ_NAME -p tcp --dport 443 -j SNAT --to-source $FW_INT_DMZ_ADDR
-iptables -A FORWARD -i $FW_INT_PUB_NAME -o $FW_INT_DMZ_NAME -p tcp --dport 443 -d $WEBSRV_INT_DMZ_ADDR -j ACCEPT
-iptables -t nat -A POSTROUTING -s $DMZ_NET -o $FW_INT_PUB_NAME -j SNAT --to-source $FW_INT_PUB_ADDR
+####
+# iptables -t nat -I PREROUTING -i $FW_INT_PUB_NAME -p tcp --dport 443 -j DNAT --to-destination $WEBSRV_INT_DMZ_ADDR
+# iptables -t nat -I POSTROUTING -o $FW_INT_DMZ_NAME -p tcp --dport 443 -j SNAT --to-source $FW_INT_DMZ_ADDR
+# iptables -A FORWARD -i $FW_INT_PUB_NAME -o $FW_INT_DMZ_NAME -p tcp --dport 443 -d $WEBSRV_INT_DMZ_ADDR -j ACCEPT
+# iptables -t nat -A POSTROUTING -s $DMZ_NET -o $FW_INT_PUB_NAME -j SNAT --to-source $FW_INT_PUB_ADDR
+####
